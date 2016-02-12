72823e2b0c06185b7f2e357a8599b399c5e2b2a0
Andrew Huynh <a5.t.huynh@gmail.com>
2016-02-11 17:29:52 -0800

    Add helpful utility functions to MockEtcd

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,12 +3,28 @@
 # @Author: ahuynh
 # @Date:   2016-01-28 10:04:58
 # @Last Modified by:   ahuynh
-# @Last Modified time: 2016-02-11 14:30:08
+# @Last Modified time: 2016-02-11 17:25:54
+import etcd
 
 
 class MockEtcd( object ):
-    def delete( self, value ):
-        pass
 
-    def write( self, value, ttl ):
-        pass
+    def __init__( self, raise_exception=False ):
+        self._reset()
+        self.raise_exception = raise_exception
+
+    def _check_raise( self ):
+        if self.raise_exception:
+            raise etcd.EtcdException( 'Test Exception' )
+
+    def _reset( self ):
+        self.deleted = set([])
+        self.written = {}
+
+    def delete( self, key ):
+        self._check_raise()
+        self.deleted.add( key )
+
+    def write( self, key, value, ttl ):
+        self._check_raise()
+        self.written[ key ] = value
